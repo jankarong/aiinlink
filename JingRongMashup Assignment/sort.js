3f860fb6cef60cbfed9beb35fea819cb3283dea9
jankarong <jankarong@gmail.com>
2024-06-14 15:50:03 -0700

    cancel sort
    cancel sort

--- JingRongMashup Assignment/sort.js
+++ JingRongMashup Assignment/sort.js
@@ -1,19 +1,19 @@
 
-// Initialize Isotope
-var $grid = $('.gallery-container').isotope({
-    itemSelector: 'figure',
-    layoutMode: 'fitRows'
-});
+// // Initialize Isotope
+// var $grid = $('.gallery-container').isotope({
+//     itemSelector: 'figure',
+//     layoutMode: 'fitRows'
+// });
 
-// Filter items on button click
-$('.filter-button').on('click', function () {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-});
+// // Filter items on button click
+// $('.filter-button').on('click', function () {
+//     var filterValue = $(this).attr('data-filter');
+//     $grid.isotope({ filter: filterValue });
+// });
 
 
-$('.gallery-container').isotope({
-    // options
-    itemSelector: '.figure',
-    layoutMode: 'fitRows'
-});
+// $('.gallery-container').isotope({
+//     // options
+//     itemSelector: '.figure',
+//     layoutMode: 'fitRows'
+// });
